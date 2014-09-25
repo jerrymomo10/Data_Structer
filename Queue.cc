f04c035b62088e7bac9a417866552db560e0293e
Jerry <443956917@qq.com>
2014-09-25 16:43:02 +0800

    finish the Queue class

--- Queue.cc
+++ Queue.cc
@@ -1,1 +1,51 @@
-
+#include "Queue.h"
+Queue::Queue(int MaxQueueSize)
+{
+    front=0;
+    rear=0;
+    MaxSize=MaxQueueSize+1;
+    queue=new int[MaxSize];
+}
+bool Queue::First(int& x)const
+{
+    if(rear==front)//empty
+    {
+        return false;
+    }
+    return queue[(front+1)%MaxSize];
+}
+bool Queue::Last(int& x)const
+{
+    if(rear==front)return false;
+    return queue[rear];
+}
+Queue& Queue::Add(const int& x)
+{
+    if(!IsFull())
+    {
+        rear=(rear+1)%MaxSize;
+        queue[rear]=x;
+    }
+    return *this;
+}
+Queue& Queue::Delete(int& x)
+{
+    if(!IsEmpty())
+    {
+        front=(front+1)%MaxSize;
+        x=queue[front];
+    }
+    return *this;
+}
+void Queue::Output(ostream& out)const
+{
+    if(!IsEmpty())
+    {
+        int j=(rear+1)%MaxSize;
+        int i=(front+1)%MaxSize;
+        for(;i!=j;i=(i+1)%MaxSize)
+        {
+            out<<queue[i]<<" ";
+        }
+    }
+}
--- Queue.h
+++ Queue.h
@@ -1,19 +1,22 @@
 #ifndef _QUEUE_H_
 #define _QUEUE_H_
+#include <iostream>
+using namespace std;
 class Queue{
     public:
-    Queue(int MaxQueueSize=10);
-    ~Queue(){delete []queue;}
-    bool IsEmpty()const{return front==rear;}
-    bool IsFull()const{return front==(rear+1)%MaxSize;}
-    int First()const;
-    int Last()const;
-    Queue& Add(const int& x);
-    Queue& Delete(int& x);
+        Queue(int MaxQueueSize=10);
+        ~Queue(){delete []queue;}
+        bool IsEmpty()const{return front==rear;}
+        bool IsFull()const{return front==(rear+1)%MaxSize;}
+        bool First(int& x)const;
+        bool Last(int& x)const;
+        Queue& Add(const int& x);
+        Queue& Delete(int& x);
+        void Output(ostream& out)const;
     private:
-    int front;
-    int rear;
-    int MaxSize;
-    int* queue;
+        int front;
+        int rear;
+        int MaxSize;
+        int* queue;
 };
 #endif
