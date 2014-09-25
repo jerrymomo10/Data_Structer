7c0e155364fcd80428dd5167279bc351cf86312c
Jerry <443956917@qq.com>
2014-09-25 20:50:35 +0800

    finished LinkedQueue class

--- LinkedQueue.cc
+++ LinkedQueue.cc
@@ -1,1 +1,62 @@
-
+#include "LinkedQueue.h"
+LinkedQueue::~LinkedQueue()
+{
+    Node* next=front;
+    while(next)
+    {
+        next=front->link;
+        delete front;
+        front=next;
+    }
+}
+bool LinkedQueue::IsFull()const
+{
+    /*Node* x;
+    try{
+        x=new Node;
+        delete x;
+        return false;
+    }catch(ioexception){return true;}*/
+    return true;
+}
+bool LinkedQueue::First(int& x)const
+{
+    if(IsEmpty())return false;
+    x=front->data;
+    return true;
+}
+bool LinkedQueue::Last(int& x)const
+{
+    if(IsEmpty())return false;
+    x=rear->data;
+    return true;
+}
+LinkedQueue& LinkedQueue::Add(const int& x)
+{
+    Node* t=new Node;
+    t->data=x;
+    t->link=0;
+    if(front) rear->link=t;
+    else front=t;
+    rear=t;
+    return *this;
+}
+LinkedQueue& LinkedQueue::Delete(int& x)
+{
+    if(IsEmpty())return *this;
+    x=front->data;
+    Node* t=front;
+    front=t->link;
+    if(!front)rear=front;
+    delete t;
+    return *this;
+}
+void LinkedQueue::Output(ostream& out)const
+{
+    Node* t=front;
+    while(t)
+    {
+        out<<t->data<<" ";
+        t=t->link;
+    }
+}
--- LinkedQueue.h
+++ LinkedQueue.h
@@ -1,5 +1,7 @@
 #ifndef _LINKEDQUEUE_H_
 #define _LINKEDQUEUE_H_
+#include <iostream>
+using namespace std;
 class Node{
     friend class LinkedQueue;
     private:
@@ -8,14 +10,15 @@ class Node{
 };
 class LinkedQueue{
     public:
-        LinkedQueue(){front=rear=0};
+        LinkedQueue(){front=rear=0;}
         ~LinkedQueue();
-        bool IsEmpty()const{return front==NULL;}
+        bool IsEmpty()const{return ((front)?false:true);}
         bool IsFull()const;
         bool First(int& x)const;
         bool Last(int& x)const;
         LinkedQueue& Add(const int& x);
         LinkedQueue& Delete(int& x);
+        void Output(ostream& out)const;
     private:
         Node* front;
         Node* rear;
