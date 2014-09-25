a43ad266bb4d1514cf98e37def5be35b2b66f289
Jerry <443956917@qq.com>
2014-09-25 12:09:59 +0800

    fixed Queue.h

--- Queue.h
+++ Queue.h
@@ -1,1 +1,19 @@
-
+#ifndef _QUEUE_H_
+#define _QUEUE_H_
+class Queue{
+    public:
+    Queue(int MaxQueueSize=10);
+    ~Queue(){delete []queue;}
+    bool IsEmpty()const{return front==rear;}
+    bool IsFull()const{return front==(rear+1)%MaxSize;}
+    int First()const;
+    int Last()const;
+    Queue& Add(const int& x);
+    Queue& Delete(int& x);
+    private:
+    int front;
+    int rear;
+    int MaxSize;
+    int* queue;
+};
+#endif
