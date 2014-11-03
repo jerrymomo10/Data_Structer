af68640164d00b5a45bde80e3018abf6df5eceef
Jerry <443956917@qq.com>
2014-11-03 21:11:28 +0800

    add heapsort

--- main.cc
+++ main.cc
@@ -1,21 +1,12 @@
-#include "Chain.h"
+#include "Rank.h"
 #include <iostream>
 using namespace std;
 int main()
 {
-    Chain *l=new Chain;
-    l->Insert(0,9).Insert(0,8).Insert(0,7).Insert(0,6).Insert(0,5);
-    if(l->IsEmpty())return 0;
-    int m;
-    l->Output(cout);
-    cout<<endl;
-    l->Find(2,m);
-    cout<<"The 2th element is: "<<m<<endl;
-    cout<<"The length of the chain is: "<<l->Length()<<endl;
-    cout<<"The location of 7 is: "<<l->Search(7)<<endl;
-    l->Delete(3,m);
-    cout<<"The del element is: "<<m<<endl;
-    cout<<"The chain after del is: "<<endl;
-    l->Output(cout);
-
+    int l[]={19,18,7,6,5,4,3,2,1};
+    heapsort(l,9);
+    for(int i=0;i<(int)(sizeof(l)/sizeof(int));i++)
+    {
+        cout<<l[i]<<'\t';
+    }
 }
